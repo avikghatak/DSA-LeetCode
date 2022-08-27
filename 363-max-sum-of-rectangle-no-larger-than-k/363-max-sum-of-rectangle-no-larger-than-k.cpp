45382e15d350ac69710921a09bad92287b96ec48
Khushi Singh <71220436+khushii01@users.noreply.github.com>
2022-08-27 22:57:23 +0530

    Time: 1024 ms (88.32%), Space: 10.5 MB (97.15%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
     int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
-        int m = matrix.size(), n = matrix[0].size();
-        
-        int prefix[m+1][n+1];
-        memset(prefix,0,sizeof(prefix));
-        
-        for(int i=1; i<=m; ++i) prefix[i][1] = prefix[i-1][1] + matrix[i-1][0];
-        for(int i=1; i<=n; ++i) prefix[1][i] = prefix[1][i-1] + matrix[0][i-1];
-        
-        for(int i=2; i<=m; ++i){
-            for(int j=2; j<=n; ++j){
-                prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1] + matrix[i-1][j-1];
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int ans = INT_MIN;
+        int dp[m+1][n+1];
+        memset(dp,0,sizeof(dp));
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                dp[i][j] = matrix[i-1][j-1]+dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1];
             }
         }
-        
-        
-        int ans = INT_MIN;
-        for (int i = 1; i <= m; ++i) {
-			for (int j = 1; j <= n; ++j) {
-				for (int r = i; r <= m; ++r) {
-					for (int c = j; c <= n; ++c) {
-						int sum = prefix[r][c] - prefix[i-1][c] - prefix[r][j-1] + prefix[i-1][j-1];
-						if (sum <= k) {
-							ans = max(ans, sum);
-						}
-					}
-				}
-
-			}
-		}
-
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                for(int r=i+1;r<=m;r++){
+                    for(int c=j+1;c<=n;c++){
+                        int curr_sum = dp[r][c] - dp[r][j] - dp[i][c] + dp[i][j];
+                        if(curr_sum<=k)
+                            ans = max(ans,curr_sum);
+                    }
+                }
+            }
+        }
         return ans;
     }
 };

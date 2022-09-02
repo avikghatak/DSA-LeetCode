d3fb028d06e3660fa7f2ff1f87cade6ed00a9282
Khushi Singh <71220436+khushii01@users.noreply.github.com>
2022-09-03 00:55:42 +0530

    Time: 52 ms (6.14%), Space: 25.3 MB (52.14%) - LeetHub

--- 530-minimum-absolute-difference-in-bst/530-minimum-absolute-difference-in-bst.cpp
+++ 530-minimum-absolute-difference-in-bst/530-minimum-absolute-difference-in-bst.cpp
@@ -13,7 +13,7 @@ class Solution {
 public:
     int getMinimumDifference(TreeNode* root) {
         stack<TreeNode*> s;
-        vector<int> BSTarray;
+        int prev =-1,curr,ans=INT_MAX;
         TreeNode* temp = root;
         while(temp!=NULL || !s.empty()){
             while(temp){
@@ -22,14 +22,13 @@ public:
             }
             temp = s.top();
             s.pop();
-            BSTarray.push_back(temp->val);
+            curr = temp->val;
+            if(prev!=-1){
+                ans = min(ans,curr-prev);
+            }
+            prev=curr;
             temp = temp->right;
         }
-        int ans=INT_MAX;
-        for(int i=1;i<BSTarray.size();i++){
-            ans = min(ans,BSTarray[i]-BSTarray[i-1]);
-        }
-        
         return ans;
     }
 };
